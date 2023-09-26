a3f5dbfb04292fdced0044fe3a70013a23f1ea7a
PrikhodkoOlga <nuara.23.04.15@gmail.com>
2023-09-27 00:44:53 +0300

    complete day5 tasks

--- JavaMarathon2021/src/main/java/day5/Task1.java
+++ JavaMarathon2021/src/main/java/day5/Task1.java
@@ -2,6 +2,49 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car = new Car();
+        car.setColor("White");
+        car.setModel("BMW");
+        car.setDateRelease(2015);
+        System.out.println(car);
+    }
+}
+class Car{
+    private String model;
+    private String color;
+    private int dateRelease;
+    public String getModel(){
+        return model;
+    }
+    public String getColor(){
+        return color;
+    }
+    public int getDateRelease(){
+        return dateRelease;
+    }
+    public void setModel(String model){
+        if (model.isEmpty())
+            System.out.println("Некорректные данные при задании модели!!! Значение не может быть пустым");
+        else
+            this.model = model;
+    }
+    public void setColor(String color){
+        if (color.isEmpty())
+            System.out.println("Некорректные данные при задании цвета!!! Значение не может быть пустым");
+        else
+            this.color = color;
+    }
+    public void setDateRelease(int dateRelease){
+        if (dateRelease < 1884)
+            System.out.println("Некорректные данные при задании года выпуска!!! Самая старая машина в мире 1884 года выпуска");
+        else if (dateRelease > java.time.LocalDateTime.now().getYear())
+            System.out.println("Некорректные данные при задании года выпуска!!! Год выпуска не может быть больше текущего года");
+        else
+            this.dateRelease = dateRelease;
+    }
 
+    @Override
+    public String toString() {
+        return "Модель: " + getModel() + " Цвет: " + getColor() + " Год выпуска: " + getDateRelease();
     }
 }
--- JavaMarathon2021/src/main/java/day5/Task2.java
+++ JavaMarathon2021/src/main/java/day5/Task2.java
@@ -3,6 +3,31 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motorbike = new Motorbike("Kawasaki Ninja ZX-10R", "Green", 2017);
+        System.out.println(motorbike);
+    }
+}
+class Motorbike{
+    private String model;
+    private String color;
+    private int dateRelease;
 
+    public Motorbike(String model, String color, int dateRelease){
+        this.model = model;
+        this.color = color;
+        this.dateRelease = dateRelease;
+    }
+    public String getModel(){
+        return model;
+    }
+    public String getColor(){
+        return color;
+    }
+    public int getDateRelease(){
+        return dateRelease;
+    }
+    @Override
+    public String toString() {
+        return "Модель: " + getModel() + " Цвет: " + getColor() + " Год выпуска: " + getDateRelease();
     }
 }
